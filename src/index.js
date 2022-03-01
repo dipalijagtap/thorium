b78e0f23facd1ce7f8616986bef659cfccd7cb67
dipalijagtap <dipalijagtap06@gmail.com>
2022-03-01 22:00:34 +0530

    part2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://dipali_jagtap:*****@cluster0.8z64u.mongodb.net/dipali?authSource=admin&replicaSet=atlas-w2vzbl-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
